95844d751d12dc5d561bac5043fe78697d028884
alan <alanarostegui@hotmail.com>
2020-04-25 22:14:06 -0500

    feature(toolbar): create component

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Toolbar = props => ( 
+    <header>
+        <nav>
+            <div></div>
+            <div><a href="/">The logo</a></div>
+            <div>
+                <ul>
+                    <li><a href="/">Products</a></li>
+                    <li><a href="/">Users</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+ );
+ 
+export default Toolbar;
