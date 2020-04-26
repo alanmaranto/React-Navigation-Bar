acc78406d0e8f6b55c2885c62247c2a18c5b1184
alan <alanarostegui@hotmail.com>
2020-04-25 22:56:39 -0500

    fix(app): missing sidedrawer in main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Toolbar from "./components/Toolbar/Toolbar";
+import SideDrawer from './components/SideDrawer/SideDrawer';
+
 function App() {
   return (
-    <div className="App">
+    <div style={{ height: '100%'}}>
       <Toolbar />
+      <SideDrawer />
       <main style={{ marginTop: '64px'}}>
         <p>Page content</p>
       </main>
