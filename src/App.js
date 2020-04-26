3d1142a90e699ebb7f3b21ab81071465e0794963
alan <alanarostegui@hotmail.com>
2020-04-25 22:33:50 -0500

    style(toolbar): style toolbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import Toolbar from './components/Toolbar/Toolbar';
+import React from "react";
+import Toolbar from "./components/Toolbar/Toolbar";
 function App() {
   return (
     <div className="App">
       <Toolbar />
+      <main style={{ marginTop: '64px'}}>
+        <p>Page content</p>
+      </main>
     </div>
   );
 }
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React from "react";
+import "./Toolbar.css";
 
-const Toolbar = props => ( 
-    <header>
-        <nav>
-            <div></div>
-            <div><a href="/">The logo</a></div>
-            <div>
-                <ul>
-                    <li><a href="/">Products</a></li>
-                    <li><a href="/">Users</a></li>
-                </ul>
-            </div>
-        </nav>
-    </header>
- );
- 
+const Toolbar = (props) => (
+  <header className="toolbar">
+    <nav className="toolbar__navigation">
+      <div/>
+      <div className="toolbar__logo">
+        <a href="/">The logo</a>
+      </div>
+      <div className="spacer"/>
+      <div className="toolbar_navigation-items">
+        <ul>
+          <li>
+            <a href="/">Products</a>
+          </li>
+          <li>
+            <a href="/">Users</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
 export default Toolbar;
