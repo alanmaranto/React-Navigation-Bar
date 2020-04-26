17fb23ee4c1ca5b32f98d4dc62d3fa8afc67339d
alan <alanarostegui@hotmail.com>
2020-04-25 22:55:34 -0500

    feature(sidedrawer): add component

---
+++ src/components/SideDrawer/SideDrawer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./SideDrawer.css";
+
+const SideDrawer = (props) => (
+  <nav className="side-drawer">
+    <ul>
+      <li>
+        <a href="/">Products</a>
+      </li>
+      <li>
+        <a href="/"></a>Users
+      </li>
+    </ul>
+  </nav>
+);
+
+export default SideDrawer;
